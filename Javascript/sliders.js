942dbdc09a458338e364556094292835c1c9b090
karina-talibzhanova <karina.talibzhanova@durham.ac.uk>
2019-01-12 17:10:34 +0000

    appropriate inputs on html page that actually work

--- Javascript/sliders.js
+++ Javascript/sliders.js
@@ -2,11 +2,45 @@
 var time_period = document.getElementById("time_period");
 
 time_period.oninput = function() {
-    time_period.value = this.value;
-    setup();
+    noLoop();
+    obj.timePeriod = this.value;
+    loop();
 }
 
 
+var wavelength = document.getElementById("wavelength");
+
+wavelength.oninput = function() {
+    noLoop();
+    obj.wavelength = this.value;
+    loop();
+}
+
+var wave_text = document.getElementById("wave_text");
+
+wave_text.oninput = function() {
+    noLoop();
+    obj.text = this.value;
+    loop();
+}
+
+
+var text_size = document.getElementById("text_size");
+
+text_size.oninput = function() {
+    noLoop();
+    obj.size = parseInt(this.value);
+    loop();
+}
+
+
+var background_col = document.getElementById("background_col");
+
+background_col.oninput = function() {
+    noLoop();
+    obj.backgroundColor = this.value;
+    loop();
+}
 
 var obj;
 var pg;
@@ -16,7 +50,7 @@ function setup(){
 	canvas = createCanvas(600, 600);
 	canvas.parent('test');
 	pg = createGraphics(400, 400);
-	obj = new StringWave("Hello World!", color(0, 0, 255), 70, time_period.value, 150.5);
+	obj = new StringWave("Hello World!");
 }
 
 function draw(){
--- Javascript/stringwave.js
+++ Javascript/stringwave.js
@@ -1,11 +1,3 @@
-//put canvas in predefined place on page (div). need to know how to control canvas (look on p5)
-//ability to start and stop sketch
-
-//string, int, float (between 0.05 and 0.3), int, string (rgb, rgba, hex)
-
-
-
-
 class StringWave {
     constructor(user_text, user_background, user_size, user_time, user_wavelength){
 		this.t = user_text; // actual text
@@ -13,24 +5,59 @@ class StringWave {
 		this.hue = 0; // changes start colour
 		this.text_size = user_size || 70;
 		this.time_period = user_time || 0.15;
-		this.wavelength = user_wavelength || 800;
-		this.background = user_background || 0;
-    }
+		this.string_wavelength = user_wavelength || 800;
+		this.background_color = user_background || 0;
+	}
+	
+	get text() {
+		return this.t;
+	}
+
+	set text(value) {
+		this.t = value;
+	}
+
+
+	get timePeriod() {
+		return this.time_period;
+	}
+
+	set timePeriod(value) {
+		this.time_period = value;
+	}
+
+
+	get size() {
+		return this.text_size;
+	}
+
+	set size(value) {
+		this.text_size = value;
+	}
+
+
+	get wavelength() {
+		return this.string_wavelength;
+	}
+
+	set wavelength(value) {
+		this.string_wavelength = value;
+	}
+
+
+	get backgroundColor() {
+		return this.background_color;
+	}
+
+	set backgroundColor(value) {
+		this.background_color = value;
+	}
+
 
     draw(pg){
-		pg.background(this.background);
+		pg.background(this.background_color);
 
 		pg.colorMode(HSB);
-
-		// // canvas size
-		// // canvas colour
-		// // text colour
-		// // i think it would be easier for users to input hex codes for colours bc i cba with a colour picker or something
-		// // actual text itself
-
-		// // use sliders for stuff
-
-		  // just changes the background colour of the whole page, not the canvas. probably can remove
 		
 		pg.textSize(this.text_size); // text size/height measured in pixels
 		let startX = (pg.width - pg.textWidth(this.t)) / 2;
@@ -40,14 +67,14 @@ class StringWave {
 			this.hue = (this.hue + 5) % 360; //this bit just changes the colour of the text so it cycles in a rainbow fashion
 		}
 		
-		// // this bit monitors the user's mouse to determine how wavy and fast it should move
+		// this bit monitors the user's mouse to determine how wavy and fast it should move
 		for (let i = 0; i < this.t.length; i++) {
 			let chr = this.t.charAt(i);
 			let y = pg.height / 2 + (sin(a) * mouseY); // this bit makes it super duper wavy
 			pg.fill(this.hue, 100, 100, 0.8); // so i think it just redraws each letter and colours it in with the hue calculated earlier (h) ? HSB vals + opacity
 			pg.text(chr, currentX, y); //displays the character at a certain coordinate
 			currentX += pg.textWidth(chr);
-			a += mouseX / this.wavelength; //this changes how 'wavy' the sine wave is. bigger numbers means flatter sine wave i.e. basically the period of the wave
+			a += mouseX / this.string_wavelength; //this changes how 'wavy' the sine wave is. bigger numbers means flatter sine wave i.e. basically the period of the wave
 		}
 		
 		this.angle -= this.time_period; //this changes the time period of the wave. bigger numbers means faster wave. good values are between 0.05 and 0.3
